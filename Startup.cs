a7ca6b022bc30ce3fb65542ed3fcf26e092762ad
北 <87887755+Clare0478@users.noreply.github.com>
2022-12-27 17:18:59 +0800

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HAVE_IIdentity.Startup))]
+namespace HAVE_IIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
